fa4381184dea86c859509d676cbc1b59734a586b
Lee-Ha Young <98532217+Rumisister@users.noreply.github.com>
2022-06-08 00:34:27 +0900

    3장 컴포넌트

---
+++ hello-react/src/Say.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+const Say = () => {
+  const [message, setMessage] = useState("");
+  const onClickEnter = () => setMessage("하이");
+  const onClickLeave = () => setMessage("수고");
+
+  const [color, serColor] = useState("black");
+
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: "teal" }} onClick={() => serColor("teal")}>
+        teal
+      </button>
+      <button style={{ color: "orange" }} onClick={() => serColor("orange")}>
+        orange
+      </button>
+      <button style={{ color: "blue" }} onClick={() => serColor("blue")}>
+        blue
+      </button>
+    </div>
+  );
+};
+
+export default Say;
