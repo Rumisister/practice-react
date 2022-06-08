4bbc19785edf3c68df662065302c0dbdd770add3
Lee-Ha Young <98532217+Rumisister@users.noreply.github.com>
2022-06-08 22:16:37 +0900

    4장 이벤트핸들링

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import Router from "./Router";
+
+ReactDOM.render(<Router />, document.getElementById("root"));
