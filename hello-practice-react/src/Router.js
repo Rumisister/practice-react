ced84eb709dfb8a1cffce783674a36721eb3e61e
Lee-Ha Young <98532217+Rumisister@users.noreply.github.com>
2022-06-11 18:50:42 +0900

    4장 DOM이름짓기

--- hello-practice-react/src/Router.js
+++ hello-practice-react/src/Router.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Event from "./EventHandle/App";
 import App from "./App";
+import DOM from "./DOM/App";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-
         <Route path="/Event" element={<Event />} />
+        <Route path="/DOM" element={<DOM />} />
       </Routes>
     </BrowserRouter>
   );
